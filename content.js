e6c730c926dea10b4a15f8c953b342cbc74ce802
zrzava <gabrielaprazska@gmail.com>
2025-02-22 15:44:44 +0100

    Update content.js

--- content.js
+++ content.js
@@ -197,7 +197,7 @@ function displayProduct(product) {
     }
 
     const productHTML = `
-        <div class="section item" style="margin-top: -40px;">
+        <div class="section item">
             <div class="section-content item-text">
                 <h1 id="product-name">${product.name_en}</h1>
                 <p id="product-description">${product.description_en}</p>
